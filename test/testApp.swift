ef976d70d5db413787ab8b8ecfcab6a7908125e2
Wang Xin <wang._xin@icloud.com>
2022-01-09 17:26:58 +0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 王鑫 on 2022/1/9.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
